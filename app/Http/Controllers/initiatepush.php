c97718178f7d890864eed6d609ed6f715f34b246
keronei <keronei95@gmail.com>
2018-12-24 13:50:17 +0300

    getting status response

--- app/Http/Controllers/initiatepush.php
+++ app/Http/Controllers/initiatepush.php
@@ -21,6 +21,9 @@ class initiatepush extends Controller
         $stkPushSimulation=$mpesa->STKPushSimulation(174379, 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919', 'CustomerPayBillOnline', $Amount, $phoneNumber, 174379, $phoneNumber, $CallBackURL, 'lozadasuplies', 'lozada', 'Payment');
 
         echo "Processing . . .";
+        
+        
+        Log::error('INITIATION RESPONSE FROM MPESA: '.$stkPushSimulation);
            
     }
     
