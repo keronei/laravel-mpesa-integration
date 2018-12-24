aab1344ef524112eeea09dceec97ff0aec15eec9
keronei <keronei95@gmail.com>
2018-12-24 10:15:14 +0300

    Attempt to get callbackdata immediatelly

--- app/Http/Controllers/initiatepush.php
+++ app/Http/Controllers/initiatepush.php
@@ -21,5 +21,15 @@ class initiatepush extends Controller
         $stkPushSimulation=$mpesa->STKPushSimulation(174379, 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919', 'CustomerPayBillOnline', $Amount, $phoneNumber, 174379, $phoneNumber, $CallBackURL, 'lozadasuplies', 'lozada', 'Payment');
 
         echo "Processing . . .";
+        
+        $callbackData=$mpesa->getDataFromCallback();
+        
+        if(!empty($callbackData)){
+             Log::error('IMEDIATE CALLBACK DATA: '.$callbackData);
+        }
+    }
+    
+    public function finishProcess(){
+        //
     }
 }
