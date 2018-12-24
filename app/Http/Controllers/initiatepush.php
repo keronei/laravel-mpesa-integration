5ddd1c9c8a9ca3c1428c91344cb5a036139766a4
keronei <keronei95@gmail.com>
2018-12-24 11:21:27 +0300

    removed clutter

--- app/Http/Controllers/initiatepush.php
+++ app/Http/Controllers/initiatepush.php
@@ -21,12 +21,7 @@ class initiatepush extends Controller
         $stkPushSimulation=$mpesa->STKPushSimulation(174379, 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919', 'CustomerPayBillOnline', $Amount, $phoneNumber, 174379, $phoneNumber, $CallBackURL, 'lozadasuplies', 'lozada', 'Payment');
 
         echo "Processing . . .";
-        
-        $callbackData=$mpesa->getDataFromCallback();
-        
-        if(!empty($callbackData)){
-             Log::error('IMEDIATE CALLBACK DATA: '.$callbackData);
-        }
+           
     }
     
     public function finishProcess(){
