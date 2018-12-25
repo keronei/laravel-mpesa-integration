8c69dcbdc9dddb34141d05f54714be2ecc7b87f5
keronei <keronei95@gmail.com>
2018-12-25 07:57:02 +0300

    results page

---
+++ resources/views/waiting.blade.php
@@ -0,0 +1,121 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>MPesa Pay Result</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        
+        <style>
+          html, body {
+                background-color: #fff;
+                color: #636b6f;
+                padding-top:80px;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 100;
+                height: 80%;
+                margin: 0;
+               
+            }
+             .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 25px;
+                padding:20px;
+            }
+            
+            .send {
+                background-color:#b3e6b3;
+                height:40px;
+                width:150px;
+                border-radius:60px;
+                font-family: 'Raleway', sans-serif;
+                font-weight: 150;
+                font-size:15px;
+            }
+        </style>
+            <script>
+            var CheckoutRequestID = '<?= $CheckoutRequestID ?>';
+            var status = '<?= $complete ?>';
+                var complete = status;
+                while(!complete){
+                setInterval(function getStatus() {
+                     $.ajaxSetup({
+                    headers: {
+                      'X-CSRF-TOKEN': $('meta[name="_token"]').attr('content')
+                                }
+                            });
+                    $.ajax({
+                        url: 'https://integrate-payment.herokuapp.com/check/'+CheckoutRequestID,
+                        method: 'GET',
+                        async: true,
+                        success: function(data) {
+                            if (data.status == "success") {
+ 
+                               console.log(data);
+                               
+                               switch(data) {
+                                    case 0:
+                                      console.log('PAID');
+                                      window.alert('payment accepted!');
+                                      complete = true;
+                                      break;
+                                    case 1:
+                                      console.log('pending...');
+                                      break;
+                                    case 2:
+                                      console.log('Unfortunately payment failed');
+                                      window.alert('Rejected payment');
+                                      complete = true;
+                                    break;
+                                    default:
+                                      console.log('stray result..');
+                                      complete = true;
+                                  }
+                             
+                            }
+                           
+                        }
+                    });
+                },2000);
+                }
+                if(status){
+                    window.alert('Initiation rejected your payment');
+                    console.log('Payment failed to initiate');
+                }
+                
+            
+</script>
+    </head>
+        <body>
+        <div class="content">
+           <div class="title m-b-md">
+                    Your payment is being processed. Continue shopping...
+                </div>
+
+        </div>
+        <script src="http://code.jquery.com/jquery-3.3.1.min.js"</script>
+        </body>
+</html>        
