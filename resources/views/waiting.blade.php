ee35bdbfbb5c8d6273f361758ec4a08aa6878ad5
keronei <keronei95@gmail.com>
2018-12-25 17:37:08 +0300

    closed script tag , silly bug

--- resources/views/waiting.blade.php
+++ resources/views/waiting.blade.php
@@ -9,7 +9,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-        <script src="http://code.jquery.com/jquery-3.3.1.min.js"</script>
+        <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
         <style>
           html, body {
                 background-color: #fff;
